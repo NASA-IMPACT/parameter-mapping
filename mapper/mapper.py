212e5454dbb6d0c62cc1776f0e99eec7e2e7ddb7
xhagrg <grg.iksha@hotmail.com>
2016-12-19 16:01:59 -0600

    Revert back to normal.

--- mapper/mapper.py
+++ mapper/mapper.py
@@ -8,8 +8,7 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for, R
 from werkzeug import secure_filename
 import os
 import json
-# from bson import json_util
-import bson
+from bson import json_util
 import time
 import operator
 from bson.objectid import ObjectId
